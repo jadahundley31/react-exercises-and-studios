9827dde66213b57006070041077b14bb216cae9f
Jada Hundley <jadahundley@gmail.com>
2023-07-30 17:30:25 -0500

    updated booklist and added pictures

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "Lovely Books";
+   let pageTitle = "My favorite Books";
    let book1 = <a href="https://m.media-amazon.com/images/I/413EAYqKuSL._SX331_BO1,204,203,200_.jpg"/> ;
-   let book2 = "";
-   let book3 = "";
+   let book2 = <a href="https://m.media-amazon.com/images/I/510toWaUfNL._SY346_.jpg"/>;
+   let book3 = <a href="https://m.media-amazon.com/images/I/515LvF4rxkL.jpg"/>;
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Love is a Beast" />
+         <img src={book2} alt="Truth in the Ashes" />
+         <img src={book3} alt="Strange Justice" />
       </div>      
    );
 }
