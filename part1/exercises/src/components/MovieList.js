80aa150fba4cccae32db6022ca5bb2d0cce81e7b
Jada Hundley <jadahundley@gmail.com>
2023-07-30 16:21:34 -0500

    added two more movie titles

--- part1/exercises/src/components/MovieList.js
+++ part1/exercises/src/components/MovieList.js
@@ -3,7 +3,8 @@ import classes from './MovieList.module.css';
 
 class MovieList extends React.Component {
    render() {
-      const movies = ["Pride and Prejudice", "Howl's Moving Castle", "Legally Blonde", "Star Wars"];
+      const movies = ["Pride and Prejudice", "Howl's Moving Castle", "Legally Blonde", "Star Wars", "The Little Mermaid",
+   "Lion King"];
       return (
       <div>
          <h3 className = {classes.movieHeading}>Movies I Watched This Month</h3>
@@ -12,6 +13,8 @@ class MovieList extends React.Component {
             <li className={classes.movieText}>{movies[1]}</li>
             <li className={classes.movieText}>{movies[2]}</li>
             <li className={classes.movieText}>{movies[3]}</li>
+            <li className={classes.movieText}>{movies[4]}</li>
+            <li className={classes.movieText}>{movies[5]}</li>
          </ol>
       </div>
       );
